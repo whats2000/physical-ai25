30c66e9128daa196d92b46443f5b6256b1aaf262
whats2000 <whats2000mc@gmail.com>
2025-10-09 10:40:00 +0800

    Feat: Add more detail explanation hint to reconstruction

--- hw1/reconstruct.py
+++ hw1/reconstruct.py
@@ -1,51 +1,135 @@
+import argparse
+from typing import Tuple
+
 import numpy as np
 import open3d as o3d
-import argparse
 
 
-def depth_image_to_point_cloud(rgb, depth):
+def depth_image_to_point_cloud(
+    rgb_image: np.ndarray, 
+    depth_image: np.ndarray
+) -> o3d.geometry.PointCloud:
+    """
+    Convert RGB and depth images to a point cloud.
+    
+    :param rgb_image: RGB image array with shape (height, width, 3)
+    :param depth_image: Depth image array with shape (height, width)
+    :return: Point cloud generated from the RGB-D images
+    
+    Note:
+        - Camera uses pinhole model with resolution 512x512
+        - Horizontal and vertical FOV are 90 degrees
+        - depth_scale = 1000
+    """
     # TODO: Get point cloud from rgb and depth image 
     raise NotImplementedError
-    return pcd
+    return point_cloud
 
 
-def preprocess_point_cloud(pcd, voxel_size):
+def preprocess_point_cloud(
+    point_cloud: o3d.geometry.PointCloud, 
+    voxel_size: float
+) -> o3d.geometry.PointCloud:
+    """
+    Downsample point cloud using voxelization.
+    
+    :param point_cloud: Input point cloud to be downsampled
+    :param voxel_size: Size of voxels for downsampling
+    :return: Downsampled point cloud with reduced number of points
+    """
     # TODO: Do voxelization to reduce the number of points for less memory usage and speedup
     raise NotImplementedError
-    return pcd_down
+    return downsampled_point_cloud
 
 
-def execute_global_registration(source_down, target_down, source_fpfh,
-                                target_fpfh, voxel_size):
+def execute_global_registration(
+    source_downsampled: o3d.geometry.PointCloud, 
+    target_downsampled: o3d.geometry.PointCloud, 
+    source_features: o3d.pipelines.registration.Feature,
+    target_features: o3d.pipelines.registration.Feature, 
+    voxel_size: float
+) -> o3d.pipelines.registration.RegistrationResult:
+    """
+    Perform global registration between two point clouds.
+    
+    :param source_downsampled: Source point cloud (downsampled)
+    :param target_downsampled: Target point cloud (downsampled)
+    :param source_features: FPFH features of source point cloud
+    :param target_features: FPFH features of target point cloud
+    :param voxel_size: Voxel size used for downsampling
+    :return: Registration result containing transformation matrix
+    """
+    # TODO: Apply global registration as initialization for local methods
     raise NotImplementedError
-    return result
+    return registration_result
 
 
-def local_icp_algorithm(source_down, target_down, trans_init, threshold):
-    # TODO: Use Open3D ICP function to implement
+def local_icp_algorithm(
+    source_downsampled: o3d.geometry.PointCloud, 
+    target_downsampled: o3d.geometry.PointCloud, 
+    initial_transformation: np.ndarray, 
+    distance_threshold: float
+) -> o3d.pipelines.registration.RegistrationResult:
+    """
+    Perform local ICP registration using Open3D library.
+    
+    :param source_downsampled: Source point cloud (downsampled)
+    :param target_downsampled: Target point cloud (downsampled)
+    :param initial_transformation: Initial transformation matrix (4x4)
+    :param distance_threshold: Maximum correspondence distance threshold
+    :return: Registration result containing refined transformation matrix
+    """
+    # TODO: Use Open3D ICP function to implement (point-to-point or point-to-plane)
     raise NotImplementedError
-    return result
+    return registration_result
 
 
-def my_local_icp_algorithm(source_down, target_down, trans_init, voxel_size):
+def my_local_icp_algorithm(
+    source_downsampled: o3d.geometry.PointCloud, 
+    target_downsampled: o3d.geometry.PointCloud, 
+    initial_transformation: np.ndarray, 
+    voxel_size: float
+) -> o3d.pipelines.registration.RegistrationResult:
+    """
+    Perform local ICP registration with custom implementation.
+    
+    :param source_downsampled: Source point cloud (downsampled)
+    :param target_downsampled: Target point cloud (downsampled)
+    :param initial_transformation: Initial transformation matrix (4x4)
+    :param voxel_size: Voxel size for correspondence finding
+    :return: Registration result containing refined transformation matrix
+    
+    Reference:
+        https://cs.gmu.edu/~kosecka/cs685/cs685-icp.pdf
+    """
     # TODO: Write your own ICP function
     raise NotImplementedError
-    return result
+    return registration_result
 
 
-def reconstruct(args):
+def reconstruct(args: argparse.Namespace) -> Tuple[o3d.geometry.PointCloud, np.ndarray]:
+    """
+    Reconstruct 3D scene from RGB-D image sequence.
+    
+    :param args: Command line arguments containing version ('open3d' or 'my_icp') and data_root path
+    :return: Tuple of (reconstructed_point_cloud, predicted_camera_positions)
+    
+    Pipeline:
+        1. Unproject depth images at time t_i and t_{i+1} to point clouds
+        2. Apply voxelization to downsample point clouds
+        3. Apply global registration for initialization
+        4. Apply local registration (ICP) to obtain transformation matrix
+        5. Align point clouds and accumulate transformations along trajectory
+    
+    Example:
+        if args.version == 'open3d':
+            transformation = local_icp_algorithm(...)
+        elif args.version == 'my_icp':
+            transformation = my_local_icp_algorithm(...)
+    """
     # TODO: Return results
-    """
-    For example:
-        ...
-        args.version == 'open3d':
-            trans = local_icp_algorithm()
-        args.version == 'my_icp':
-            trans = my_local_icp_algorithm()
-        ...
-    """
     raise NotImplementedError
-    return result_pcd, pred_cam_pos
+    return reconstructed_point_cloud, predicted_camera_positions
 
 
 if __name__ == '__main__':
@@ -61,22 +145,33 @@ if __name__ == '__main__':
         args.data_root = "data_collection/second_floor/"
     
     # TODO: Output result point cloud and estimated camera pose
-    '''
+    """
     Hint: Follow the steps on the spec
-    '''
-    result_pcd, pred_cam_pos = reconstruct()
+    """
+    reconstructed_point_cloud, predicted_camera_positions = reconstruct(args)
 
     # TODO: Calculate and print L2 distance
-    '''
+    """
     Hint: Mean L2 distance = mean(norm(ground truth - estimated camera trajectory))
-    '''
-    print("Mean L2 distance: ", )
+    
+    Steps:
+        1. Load ground truth camera positions from data
+        2. Compute L2 distance: ||ground_truth_position - predicted_position||_2 for each frame
+        3. Calculate mean of all L2 distances
+    """
+    ground_truth_camera_positions = None  # TODO: Load ground truth
+    mean_l2_distance = None  # TODO: Calculate mean L2 distance
+    print("Mean L2 distance: ", mean_l2_distance)
 
     # TODO: Visualize result
-    '''
-    Hint: Sould visualize
-    1. Reconstructed point cloud
-    2. Red line: estimated camera pose
-    3. Black line: ground truth camera pose
-    '''
-    o3d.visualization.draw_geometries()
+    """
+    Visualization requirements:
+        1. Reconstructed point cloud (remove ceiling for better view)
+        2. Red line: estimated camera trajectory
+        3. Black line: ground truth camera trajectory
+    
+    Note: Transform trajectories and 3D scene to same coordinate system
+          (be aware of coordinate direction and scale)
+    """
+    visualization_geometries = []  # TODO: Add geometries to visualize
+    o3d.visualization.draw_geometries(visualization_geometries)
