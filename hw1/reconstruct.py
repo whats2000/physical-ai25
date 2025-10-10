7555d5d6ac8cad39f6f86fc4f1fb8035d7822938
whats2000 <whats2000mc@gmail.com>
2025-10-10 13:46:26 +0800

    Feat: Reimplement 3D point cloud

--- hw1/reconstruct.py
+++ hw1/reconstruct.py
@@ -1,12 +1,45 @@
-import numpy as np
-import open3d as o3d
 import argparse
 
+import numpy as np
+import open3d as o3d
 
-def depth_image_to_point_cloud(rgb, depth):
-    # TODO: Get point cloud from rgb and depth image
-    raise NotImplementedError
-    return pcd
+
+def depth_image_to_point_cloud(rgb: np.ndarray, depth: np.ndarray) -> o3d.geometry.PointCloud:
+    """
+    Convert RGB and depth images to point cloud
+    :param rgb: HxWx3 RGB image
+    :param depth: HxW depth image in millimeters
+    :return: Open3D point cloud object
+    """
+    # Camera intrinsic parameters
+    height, width = depth.shape
+    focal_length = width / (2 * np.tan(np.radians(90) / 2))
+    cx, cy = width / 2, height / 2
+    depth_scale = 1000.0
+    
+    # Create meshgrid for pixel coordinates
+    u, v = np.meshgrid(np.arange(width), np.arange(height))
+    
+    # Convert depth to meters
+    z = depth.astype(np.float32) / depth_scale
+    
+    # Transform u, v back to 3D camera coordinate system
+    x = (u - cx) * z / focal_length
+    y = (v - cy) * z / focal_length
+    
+    # Filter out invalid depth
+    valid = (z > 0) & (z < 10.0)
+    
+    # Stack coordinates
+    points = np.stack([x[valid], y[valid], z[valid]], axis=-1)
+    colors = rgb[valid].astype(np.float32) / 255.0
+    
+    # Create point cloud
+    point_cloud = o3d.geometry.PointCloud()
+    point_cloud.points = o3d.utility.Vector3dVector(points)
+    point_cloud.colors = o3d.utility.Vector3dVector(colors)
+    
+    return point_cloud
 
 
 def preprocess_point_cloud(pcd, voxel_size):
@@ -34,7 +67,6 @@ def my_local_icp_algorithm(source_down, target_down, trans_init, voxel_size):
 
 
 def reconstruct(args):
-    # TODO: Return results
     """
     For example:
         ...
