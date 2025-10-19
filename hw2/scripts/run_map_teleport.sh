b477330a2859abd832844813a7d78f305bbf532d
whats2000 <whats2000mc@gmail.com>
2025-10-20 02:22:37 +0800

    Add: Add mini map interaction test

---
+++ hw2/scripts/run_map_teleport.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configure display for WSL2
+export DISPLAY=$(grep -m1 nameserver /etc/resolv.conf | awk '{print $2}'):0.0
+unset LIBGL_ALWAYS_INDIRECT
+export LIBGL_ALWAYS_SOFTWARE=1
+export GALLIUM_DRIVER=llvmpipe
+export MESA_LOADER_DRIVER_OVERRIDE=llvmpipe
+export LIBGL_DRI3_DISABLE=1
+
+# Activate habitat environment and run the interactive tool
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate habitat
+python -m src.map_teleport_interactive "$@"
